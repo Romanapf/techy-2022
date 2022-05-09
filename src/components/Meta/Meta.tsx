c8879e6475ac14e86423e8dec98060382c10eea1
romanapf17@gmail.com <MIGUELIN..1a>
2022-05-09 09:08:11 +0200

    New title

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export const Meta = () =>  {
   return (
     <Head>
-        <title>👩🏼‍💻👨🏼‍💻 Techy 2022 </title>
+        <title>Techy 2022 </title>
         <meta name="description" content="Techy 2022" />
      
       <link rel="manifest" href="../../../public/favicon.png" />
